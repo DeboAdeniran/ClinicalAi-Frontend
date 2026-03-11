994dd9786a8132fb152a827ed8b51c4a4b10c4e6
github_DeboAdeniran <raqibadeniran03@gmail.com>
2026-03-11 09:37:46 +0100

    initial commit

--- src/components/layout/PageLayout.jsx
+++ src/components/layout/PageLayout.jsx
@@ -1,10 +1,10 @@
-import Sidebar from './Sidebar';
+import Sidebar from "./Sidebar";
 export default function PageLayout({ children }) {
   return (
     <div className="flex min-h-screen bg-bg-primary">
       <Sidebar />
       {/* Desktop: offset by sidebar width. Mobile: offset by top bar height */}
-      <main className="flex-1 lg:ml-60 pt-14 lg:pt-0 p-4 lg:p-6 min-h-screen">
+      <main className="flex-1 lg:ml-60 pt-14 lg:pt-0 p-4 lg:p-6 min-h-screen mt-10">
         {children}
       </main>
     </div>
